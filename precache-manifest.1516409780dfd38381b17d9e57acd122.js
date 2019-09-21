b05f43980ae3ef7a079b505fe6f0ca1bf69ea5aa
hipdadradio <hipdadradio@gmail.com>
2019-09-21 10:15:31 -0500

    Updates

---
+++ precache-manifest.1516409780dfd38381b17d9e57acd122.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da5fd646d26dd756f53764c05f90f5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "036587888a02373f6822",
+    "url": "/static/css/main.0882e1b6.chunk.css"
+  },
+  {
+    "revision": "33b20073efd68fd48344",
+    "url": "/static/js/2.6aa4c0e5.chunk.js"
+  },
+  {
+    "revision": "036587888a02373f6822",
+    "url": "/static/js/main.96abb1db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "112df6adb5d802cc693e99137a0f4381",
+    "url": "/static/media/instalogo.112df6ad.png"
+  },
+  {
+    "revision": "ec50eb0e5c85d46f59fe43b8eb027588",
+    "url": "/static/media/logo.ec50eb0e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hipdadradio/precache-manifest.f9de823693870b4c1281eecde826dccc.js"
+  "/precache-manifest.1516409780dfd38381b17d9e57acd122.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hipdadradio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
