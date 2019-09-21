f495a0213f82140af31eee2e390f44724fef2f4c
hipdadradio <hipdadradio@gmail.com>
2019-09-21 13:22:29 -0500

    Updates to the max width of the home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,13 +7,13 @@ import { AboutContainer } from '../containers/about/AboutContainer';
 export class HomePage extends React.Component {
     render() {
         return (
-            <>
+            <div className="homeContainer">
                 <h2>You found us!</h2>
                 <h4>Here is a bit about Hip Dad Radio</h4>
                 <p>Words words words words words words words words words</p>
                 <hr />
                 <AboutContainer />
-            </>
+            </div>
         );
     }
 }
