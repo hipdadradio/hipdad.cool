3de7db64d47b84894fe77bc2bed989cd50a21358
hipdadradio <hipdadradio@gmail.com>
2019-09-21 23:25:07 -0500

    Fixing typo on homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,7 +15,7 @@ export class HomePage extends React.Component {
             <div className="homeContainer">
                 <h3>Welcome to the future of radio...</h3>
                 <YouTubeVideoContainer src="lOs6MvrmK6U" title="Grab a seat and listen up." description="" />
-                <h4 className="textContainer">Hip Dad Radio is an multimedia worldide entertainment conglomerate. Fron the paris of the plains to THE American City, head south for a little southern comfort. Echoing off the cold walls of the Berlin night clubs. An endless stream of content fuels these jets. Hip Dad Radio is the future of internet radio. Welcome.</h4>
+                <h4 className="textContainer">Hip Dad Radio is a multimedia worldide entertainment conglomerate. Fron the paris of the plains to THE American City, head south for a little southern comfort. Echoing off the cold walls of the Berlin night clubs. An endless stream of content fuels these jets. Hip Dad Radio is the future of internet radio. Welcome.</h4>
                 <MailingListFormContainer />
                 <hr />
                 <AboutContainer />
