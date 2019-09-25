1adb0622804a31d31a3dbd7eb927e1fb01dfcd69
Hip Dad Radio <hipdadradio@gmail.com>
2019-09-25 11:38:03 -0500

    Revert "Clearing README"
    This reverts commit a590a254c129c7d272e47752a89ff7fbf657f9bb.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { AboutContainer } from '../containers/about/AboutContainer';
 import { YouTubeVideoContainer } from '../containers/shows/YouTubeVideoContainer';
 import { MailingListFormContainer } from '../containers/contact/MailingListFormContainer';
-import { ScheduleContainer } from '../containers/shows/ScheduleContainer';
-
 
 /*
     Container that holds the Home page
@@ -13,7 +11,6 @@ export class HomePage extends React.Component {
     render() {
         return (
             <div className="homeContainer">
-                <ScheduleContainer />
                 <h3>Welcome to the future of radio...</h3>
                 <YouTubeVideoContainer src="YcLpNPvFoXk" title="Missed the Keynote? Watch the whole thing right here." description="" />
                 <MailingListFormContainer />
