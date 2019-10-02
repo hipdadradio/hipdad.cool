5ca025dc2977230f7319618804579a74b1b810c9
hipdadradio <hipdadradio@gmail.com>
2019-10-02 10:45:01 -0500

    Styling tweaks

--- src/containers/shows/ShowContainer.js
+++ src/containers/shows/ShowContainer.js
@@ -13,7 +13,7 @@ export class ShowContainer extends React.Component {
                 <button onClick={this.props.backToShows}>Back to Shows</button>
                 <div className="textContainer">
                     <h2>{this.props.show.title}</h2>
-                    <h4>{this.props.show.desc}</h4>
+                    <h3>{this.props.show.desc}</h3>
                 </div>
                 <hr />
                 <div>
